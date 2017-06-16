a2aa4109ff16616f148c0ff7b32a5ab9b73f5363
Avrumi Friedman <info@lakewoodprogramming.com>
2017-06-15 21:41:25 -0400

    Initial commit

---
+++ WebApplication11/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication11.Models;
+
+namespace WebApplication11.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string text)
+        {
+            HomePageViewModel vm = new HomePageViewModel();
+            if (!String.IsNullOrEmpty(text))
+            {
+                vm.CharacterCounts = CountCharsDictionary(text.ToUpper());
+                vm.Text = text;
+            }
+            return View(vm);
+        }
+
+        private Dictionary<char, int> CountCharsDictionary(string text)
+        {
+            Dictionary<char, int> result = new Dictionary<char, int>();
+            foreach (char letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+            {
+                result.Add(letter, 0);
+            }
+
+            foreach (char character in text)
+            {
+                if (result.ContainsKey(character))
+                {
+                    result[character]++;
+                }
+            }
+
+            return result;
+
+        }
+    }
+}
---
+++ WebApplication11/Models/HomePageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication11.Models
+{
+    public class HomePageViewModel
+    {
+        public Dictionary<char, int> CharacterCounts { get; set; }
+        public string Text { get; set; }
+    }
+}
